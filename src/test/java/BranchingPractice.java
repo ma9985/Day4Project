dfb5d25a2441a076be71dc3deac1ff3830576609
Mohammad <moeq24@yahoo.com>
2021-05-25 19:23:58 -0500

    Added changes to line 3 from local

--- src/test/java/BranchingPractice.java
+++ src/test/java/BranchingPractice.java
@@ -1,6 +1,13 @@
 public class BranchingPractice {
     public static void main(String[] args) {
+/*
+Akbar — Today at 7:00 PM
+https://learn.cybertekschool.com/courses/593/pages/recording-intellij-2-creating-maven-project-create-local-repository?module_item_id=36756
 
+https://learn.cybertekschool.com/courses/593/pages/recording-intellij-3-adding-gitignore-file-with-intellij-help?module_item_id=36757
+
+https://learn.cybertekschool.com/courses/593/pages/recording-intellij-4-making-commits-seeing-logs?module_item_id=36758
+ */
         // created new project
         // enabled version control
         // added .gitignore with .idea, target folder and .iml file
